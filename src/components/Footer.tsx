6ddd766fcc7a11afda4db555a6b6292ccddd64c1
HimandriSharma <himandrisharma27@gmail.com>
2023-12-02 20:11:29 +0530

    style footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,13 +39,20 @@ function Footer() {
 				Testimonials
 			</div>
 			<hr style={{ width: "100%", margin: "0 2rem" }} />
-			{/* <div style={{ display: "flex", margin: "0 0 0 1rem" }}>
-				<Image
-					preview={false}
-					src={Logo}
-					style={{ margin: "8rem 0 0 0", padding: "0 1rem 0 0" }}
-				/>
-				<div style={{ display: "flex", maxWidth: "68%", overflow: "scroll" }}>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-between",
+					margin: "0 0 0 1rem",
+				}}
+			>
+					<Image
+						preview={false}
+						src={Logo}
+						style={{ margin: "8rem 0 0 0", padding: "0 1rem 0 0" }}
+					/>
+				<div style={{ display: "flex", width: "70vw", overflow: "scroll" }}>
 					{feedbacks.map((i) => (
 						<FeedbackCard
 							name={i.name}
@@ -54,7 +61,7 @@ function Footer() {
 						/>
 					))}
 				</div>
-			</div> */}
+			</div>
 		</>
 	);
 }
