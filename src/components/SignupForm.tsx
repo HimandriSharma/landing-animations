5ca6e6824bea2a79975aad0c6c3cd911975a55d3
HimandriSharma <himandrisharma27@gmail.com>
2023-12-02 19:32:43 +0530

    signup form added

---
+++ src/components/SignupForm.tsx
@@ -0,0 +1,53 @@
+import { Button, Input } from "antd";
+import React from "react";
+
+function SignupForm() {
+	return (
+		<div
+			style={{
+				height: "100vh",
+				display: "flex",
+				flexDirection: "column",
+				width: "100%",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<div
+				style={{
+					fontSize: "2rem",
+					maxWidth: "60%",
+					fontWeight: "500",
+					color: "#B0B1B3",
+				}}
+			>
+				Sign up for exclusive access.
+			</div>
+			<Input
+				placeholder="Your email address"
+				style={{ width: "60%", height: "3rem", margin: "2rem" }}
+			/>
+			<Button
+				style={{
+					width: "60%",
+					height: "3rem",
+					background: "black",
+					color: "white",
+				}}
+			>
+				Get started
+			</Button>
+			<div
+				style={{
+					fontSize: "0.9rem",
+					fontWeight: "500",
+					margin: "2rem 0 0 0",
+				}}
+			>
+				You’ll receive an email with an invite link to join.
+			</div>
+		</div>
+	);
+}
+
+export default SignupForm;
