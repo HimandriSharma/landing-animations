21ed3330ead6a3af7f25686ad96c69313b3e3ede
HimandriSharma <himandrisharma27@gmail.com>
2023-12-02 12:33:00 +0530

    background fixed

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,10 +4,18 @@ import React from "react";
 function LandingPage() {
 	return (
 		<Row>
-			<Col xs={24} lg={16} style={{ background: "red", height: "100vh" }}>
+			<Col
+				xs={24}
+				lg={16}
+				style={{
+					background:
+						"radial-gradient(ellipse at left bottom,#1FA911, #2F15D0,#000000)",
+					height: "100vh",
+				}}
+			>
 				col-18
 			</Col>
-			<Col xs={24} lg={8} style={{ background: "blue", height: "100vh" }}>
+			<Col xs={24} lg={8} style={{ background: "#FFFFFF", height: "100vh" }}>
 				col-6
 			</Col>
 		</Row>
